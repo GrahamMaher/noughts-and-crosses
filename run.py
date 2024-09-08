f32c0ecf9ef0a6985c48dd53486de602edf3519c
Graham Maher <mahergrahamsj@gmail.com>
2024-09-08 17:57:46 +0000

    Correct syntax error Back.LIGHTBLACK_EX

--- run.py
+++ run.py
@@ -5,7 +5,7 @@ from colorama import init, Fore, Back, Style
 init(autoreset=True)
 
 X_COLOUR = Fore.LIGHTCYAN_EX + Back.LIGHTBLACK_EX
-O_COLOUR = Fore.LIGHTRED_EX + Back>LIGHTBLACK_EX
+O_COLOUR = Fore.LIGHTRED_EX + Back.LIGHTBLACK_EX
 
 def welcome_message(player_name):
     """
